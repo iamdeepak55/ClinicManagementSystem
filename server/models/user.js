3e440e275679dff9c1d5e7b0efd61b133902648c
iamdeepak55 <deepak9719558670@gmail.com>
2020-05-30 16:11:03 +0530

    first commit

---
+++ server/models/user.js
@@ -0,0 +1,9 @@
+const mongoose=require('mongoose')
+
+const Schema=mongoose.Schema
+
+const userSchema=new Schema({
+    email:String,
+    password:String
+})
+const clinic=module.exports=mongoose.model('user',userSchema,'users');
